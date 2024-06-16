f8d763318e27c317d1f8c14832a3b8d2945035be
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2024-06-16 17:36:13 +0200

    chore: cleanup

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -7,6 +7,24 @@ pub struct CreateAccount {
     pub public_key: String,
 }
 
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Account {
+    pub id: i64,
+    pub username: String,
+    pub public_key: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CreateSecret {
+    pub ciphertext: String,
+    pub enc_key: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SecretCreated {
+    pub id: String,
+}
+
 pub fn decode_public_key(payload: &str) -> Result<Vec<u8>, base64::DecodeError> {
     base64::prelude::BASE64_STANDARD.decode(payload)
 }
